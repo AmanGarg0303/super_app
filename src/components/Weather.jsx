3cd06c791627343ab33bbdfa4a839c32ee87e29b
Aman <amangarg0303@gmail.com>
2024-01-18 19:33:20 +0530

    homepage weather done

---
+++ src/components/Weather.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { rainSVG, pressure, wind, humidity } from "../assets/svgs";
+
+export const Weather = () => {
+  const [weatherData, setWeatherData] = useState();
+  const date = new Date();
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      );
+      setWeatherData(data.data);
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  console.log(weatherData);
+
+  return (
+    <div>
+      <div className="px-8 py-4 bg-pink-400 rounded-t-3xl flex gap-4 justify-around text-4xl font-semibold">
+        <p>{date?.toLocaleDateString()}</p>
+        <p>{date?.toLocaleTimeString()}</p>
+      </div>
+
+      <div className="p-8 bg-purple-500 rounded-b-3xl flex gap-5 justify-around">
+        <div className="flex flex-col gap-2 items-center">
+          {rainSVG}
+          <p className="text-2xl font-medium">
+            {weatherData?.weather[0]?.main}
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-3xl font-medium">{weatherData?.main?.temp} C</p>
+          <div className="flex">
+            <div className="mt-3">{pressure}</div>
+            <div>
+              <p>{weatherData?.main?.pressure} mbar</p>
+              <p>pressure</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex gap-2">
+            <div className="mt-3">{wind}</div>
+            <div>
+              <p>{weatherData?.wind?.speed}km/h</p>
+              <p>Wind</p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <div className="mt-3">{humidity}</div>
+            <div>
+              <p>{weatherData?.main?.humidity} %</p>
+              <p>Humidity</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import Profile from "../assets/images/profileBig.png";
+import { Weather } from "../components/Weather";
 
 export default function HomePage() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const genres = JSON.parse(localStorage.getItem("genre"));
+
   return (
-    <div>
-      <p>Home page</p>
+    <div className="bg-black h-screen w-full">
+      <div className="flex gap-x-20 text-white px-20 py-12">
+        <div className="flex-[2] flex flex-col gap-10">
+          <div className="p-8 rounded-3xl bg-blue-600 flex gap-x-10">
+            <img src={Profile} alt="profile" />
+
+            <div className="flex flex-col justify-around gap-y-5">
+              <div>
+                <p className="text-2xl">{user.name}</p>
+                <p className="text-2xl">{user.email}</p>
+                <p className="text-3xl font-semibold">{user.username}</p>
+              </div>
+
+              <div className="grid grid-cols-3">
+                {genres.map((g) => (
+                  <p
+                    key={g}
+                    className="text-lg px-6 py-2 rounded-full bg-purple-400 w-40 m-2"
+                  >
+                    {g}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <Weather />
+        </div>
+
+        <div className="flex-1 bg-green-500 rounded-3xl p-6">NEWS</div>
+      </div>
     </div>
   );
 }
